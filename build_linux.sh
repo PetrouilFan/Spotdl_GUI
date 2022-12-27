568038dd2817f27c6cb177a31d575b82e7c899a7
PetrouilFan <pfanioudakis@gmail.com>
2022-12-27 16:37:55 +0200

    Moved sources under src dir
    Moved the 2 images of the project under src directory for a
    cleaner project

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 curl https://github.com/spotDL/spotify-downloader/releases/download/v4.0.6/spotdl-4.0.6-linux -o spotdl-4.0.6-linux
 chmod +x spotdl-4.0.6-linux
 python3 -m pip install pyinstaller
-python3 -m PyInstaller --noconfirm --onefile --console --icon "spotify.ico" --add-data "spotdl-4.0.6-linux:." --add-data "spotify100.png:."  "main.py" --clean
+python3 -m PyInstaller --noconfirm --onefile --console --icon "src/spotify.ico" --add-data "spotdl-4.0.6-linux:." --add-data "src/spotify100.png:."  "main.py" --clean
 mv dist/main spotdl
 rm -rf build dist main.spec
 rm -rf spotdl-4.0.6-linux
